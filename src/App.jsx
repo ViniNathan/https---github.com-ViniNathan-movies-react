c7fb76e54d2c2f93106a2379f9dee06b71e5c84c
Nathan <you@example.com>
2024-02-01 08:38:10 -0300

    API setup and testing - all working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import {useState, useEffect} from 'react'
+import { fetchDataFromApi } from './utils/api'
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    apiTesting();
+  }, [])
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((response) => {
+        console.log(response)
+      })
+  }
+
   return (
     <div className="App">
-
+      App
     </div>
   )
 }
