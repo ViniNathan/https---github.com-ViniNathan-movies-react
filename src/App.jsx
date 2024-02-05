fb6badf35f9808805947d2867c22b779547907b2
Nathan <you@example.com>
2024-02-05 07:48:28 -0300

    Some changes in redux toolkit - setup not finished yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import {useState, useEffect} from 'react'
 import { fetchDataFromApi } from './utils/api'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => state.home);
+  console.log(url);
 
   useEffect(() => {
     apiTesting();
   }, [])
 
   const apiTesting = () => {
+    
     fetchDataFromApi('/movie/popular')
       .then((response) => {
-        console.log(response)
+        console.log(response);
+        dispatch(getApiConfiguration(response));
       })
   }
 
   return (
     <div className="App">
       App
+      {url?.total_pages}
     </div>
   )
 }
