55d5fa4ddb974dc4021be027a7176a552a7a2411
Nathan <you@example.com>
2024-02-08 10:32:34 -0300

    Add trending, switch and carousel in homePage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import HeroBanner from './heroBanner/HeroBanner'
-import './home.scss'
+import React from "react";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import "./home.scss";
 
 const Home = () => {
-  return (
-    <div className="homePage">
-      <HeroBanner />
-    </div>
-  )
-}
+    return (
+        <div className="homePage">
+            <HeroBanner />
+            <Trending />
+        </div>
+    );
+};
 
-export default Home
+export default Home;
