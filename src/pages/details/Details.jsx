1ff679f5f0d125fc4b2280e84528a45992649133
Nathan <you@example.com>
2024-02-09 10:08:26 -0300

    Add details page - finished

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './details.scss'
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+import "./details.scss";
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+    const { mediaType, id } = useParams();
+    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = useFetch(
+        `/${mediaType}/${id}/credits`
+    );
 
-export default Details
+    return (
+        <div>
+            <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+            <Cast data={credits?.cast} loading={creditsLoading} />
+            <VideosSection data={data} loading={loading} />
+        </div>
+    );
+};
+
+export default Details;
